b08edc67f59c5b5567e5a78407b39a7278956476
Maysadias09 <maysadias293@gmail.com>
2025-05-27 20:48:40 -0300

    Update AlunoRepositoryDB.java

--- src/main/java/com/mycompany/academia/repository/AlunoRepositoryDB.java
+++ src/main/java/com/mycompany/academia/repository/AlunoRepositoryDB.java
@@ -1,17 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.academia.repository;
-import com.mycompany.academia.database.Conexao;
-import com.mycompany.academia.model.Aluno;
+package repository;
+
+import database.Conexao;
+import model.Aluno;
+
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Scanner;
-
 
 public class AlunoRepositoryDB {
-     public void adicionar(Aluno aluno) {
+    public void adicionar(Aluno aluno) {
         String sql = "INSERT INTO aluno (nome, email) VALUES (?, ?)";
         try (Connection conn = Conexao.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
@@ -41,28 +37,5 @@ public class AlunoRepositoryDB {
             e.printStackTrace();
         }
         return alunos;
-    }
-    public void update(String nome,String email, String email_antigo){
-      String sql ="UPDATE aluno SET nome = ?, email = ? WHERE email =?";
-        try (Connection conn = Conexao.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)){
-            stmt.setString(1, nome);
-            stmt.setString(2, email);
-            stmt.setString(3, email_antigo);
-            stmt.executeUpdate();
-        }catch(SQLException error){}
-    }
-    public void deletar(String email){
-        String sql = "DELETE FROM aluno WHERE email =?";
-        try (Connection conn = Conexao.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)){
-            stmt.setString(1, email);
-           int linhasAfetadas = stmt.executeUpdate();
-           if(linhasAfetadas > 0){
-               System.out.println("Aluno deletado");
-           }else{
-               System.out.println("Error");
-           }
-        }catch(SQLException error){}
     }
 }

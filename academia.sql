3049eebc07375bf49a848940381dc2e666145618
Maysadias09 <maysadias293@gmail.com>
2025-05-27 19:25:51 -0300

    Update academia.sql

--- academia.sql
+++ academia.sql
@@ -1,25 +1,25 @@
--- Criação do banco de dados
-CREATE DATABASE IF NOT EXISTS academia;
-USE academia;
+CREATE DATABASE academia_db;
+USE academia_db;
 
--- Tabela de Alunos
-CREATE TABLE IF NOT EXISTS aluno (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL
+select * from aluno;
+select * from plano;
+select * from ficha_treino;
+
+CREATE TABLE aluno (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100),
+  email VARCHAR(100)
 );
 
--- Tabela de Planos
-CREATE TABLE IF NOT EXISTS plano (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    descricao TEXT
+CREATE TABLE plano (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(100),
+  descricao TEXT
 );
 
--- Tabela de Fichas de Treino
-CREATE TABLE IF NOT EXISTS ficha_treino (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    aluno_id INT,
-    descricao TEXT,
-    FOREIGN KEY (aluno_id) REFERENCES aluno(id)
+CREATE TABLE ficha_treino (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  aluno_id INT,
+  descricao TEXT,
+  FOREIGN KEY (aluno_id) REFERENCES aluno(id)
 );

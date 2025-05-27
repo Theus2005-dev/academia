97dd3da064fb7af8a148867e0e32aff16490627e
Maysadias09 <maysadias293@gmail.com>
2025-05-27 20:45:13 -0300

    Update and rename ConnectionFactory.java to Conexao.java

--- src/main/java/com/mycompany/academia/database/Conexao.java
+++ src/main/java/com/mycompany/academia/database/Conexao.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionFactory {
+public class Conexao {
     private static final String URL = "jdbc:mysql://localhost:3306/academia_db";
     private static final String USER = "root"; // seu usuário
     private static final String PASSWORD = "I1z2e3s4@"; // sua senha

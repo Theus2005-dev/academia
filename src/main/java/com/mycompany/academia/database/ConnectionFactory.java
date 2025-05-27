0680546f3a4a33b1d3a9254d0637027f5204cc0c
Maysadias09 <maysadias293@gmail.com>
2025-05-27 20:25:51 -0300

    Create ConnectionFactory.java

---
+++ src/main/java/com/mycompany/academia/database/ConnectionFactory.java
@@ -0,0 +1,15 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    private static final String URL = "jdbc:mysql://localhost:3306/academia_db";
+    private static final String USER = "root"; // seu usuário
+    private static final String PASSWORD = "I1z2e3s4@"; // sua senha
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
